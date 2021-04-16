4d0e912c119964c0a834b8f4375d41995e6cd699
Julian Vos <post@julianvos.nl>
2021-04-16 12:48:36 +0200

    Fix calculating start of week/month/year

--- app/Http/Controllers/TotalsController.php
+++ app/Http/Controllers/TotalsController.php
@@ -33,15 +33,16 @@ class TotalsController extends Controller
         switch ($scope)
         {
             case 'week':
-                $min = (new DateTime())->sub(new DateInterval('P7D'))->format('Y-m-d') . ' 00:00:00';
+                $min = (new DateTime())->sub(new DateInterval('P' . ((new DateTime())->format('w') - 1) . 'D'))->format('Y-m-d') . ' 00:00:00';
                 $max = (new DateTime())->format('Y-m-d') . ' 23:59:59';
                 break;
             case 'month':
-                $min = (new DateTime())->sub(new DateInterval('P1M'))->format('Y-m-d') . ' 00:00:00';
+                $min = (new DateTime())->sub(new DateInterval('P' . ((new DateTime())->format('j') - 1) . 'D'))->format('Y-m-d') . ' 00:00:00';
+                \Log::debug(((new DateTime())->format('m') - 1));
                 $max = (new DateTime())->format('Y-m-d') . ' 23:59:59';
                 break;
             case 'year':
-                $min = (new DateTime())->sub(new DateInterval('P1Y'))->format('Y-m-d') . ' 00:00:00';
+                $min = (new DateTime())->sub(new DateInterval('P' . ((new DateTime())->format('z') - 1) . 'D'))->format('Y-m-d') . ' 00:00:00';
                 $max = (new DateTime())->format('Y-m-d') . ' 23:59:59';
                 break;
             default:

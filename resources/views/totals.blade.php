7d33efed60525b36615807d3100b55d180765e1e
Julian Vos <post@julianvos.nl>
2021-05-11 13:58:09 +0200

    Update totals.blade.php

--- resources/views/totals.blade.php
+++ resources/views/totals.blade.php
@@ -17,7 +17,7 @@
                     </div>
                     
                     <div class="modal-body">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/BLikP6BDH5w?autoplay=1" frameborder="0"></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/BLikP6BDH5w?autoplay=1&mute=1" frameborder="0"></iframe>
                     </div>
 
                     <div class="modal-footer">

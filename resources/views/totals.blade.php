047bb80852753f9b72597a648e80a83705b55623
Julian Vos <post@julianvos.nl>
2021-05-11 13:57:16 +0200

    Update totals.blade.php

--- resources/views/totals.blade.php
+++ resources/views/totals.blade.php
@@ -17,7 +17,7 @@
                     </div>
                     
                     <div class="modal-body">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/BLikP6BDH5w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/BLikP6BDH5w?autoplay=1" frameborder="0"></iframe>
                     </div>
 
                     <div class="modal-footer">
